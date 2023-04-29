e5de57fd4bcac0d5babcf316b6362bda4ef6ee8a
Emii2003 <emile.maia@etec.sp.gov>
2023-04-28 21:00:46 -0300

    Adicionando arquivo js

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use strict";
+// console.log("Hello World")
+
+var n1 = "10"
+var n2 = "5"
+
+console.log(parseInt(n1)+parseInt(n2))
+
+var nome1 = "Dubas"
+
+if(nome1 != ""){
+    var nome1 = "Julia"
+}
+
+console.log(nome1)
