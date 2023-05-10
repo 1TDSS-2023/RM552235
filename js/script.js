4c152de7b14c41dbca5d90a85fed597b1afe653d
Emii2003 <emile.maia@etec.sp.gov.br>
2023-05-10 19:10:39 -0300

    Atualização

--- js/script.js
+++ js/script.js
@@ -1,15 +1,31 @@
 "use strict";
 // console.log("Hello World")
 
-var n1 = "10"
-var n2 = "5"
+// var n1 = "10"
+// var n2 = "5"
 
-console.log(parseInt(n1)+parseInt(n2))
+// console.log(parseInt(n1)+parseInt(n2))
 
-var nome1 = "Dubas"
+// var nome1 = "Dubas"
 
-if(nome1 != ""){
-    var nome1 = "Julia"
-}
+// if(nome1 != ""){
+//     var nome1 = "Julia"
+// }
 
-console.log(nome1)
+// console.log(nome1)
+
+const btn = document.getElementById("meu_btn")
+console.log(btn.id)
+
+btn.addEventListener("click", function(){
+    // console.log(btn.textContent)
+
+} )
+
+let r=0,g=0,b=0
+r = Math.round(Math.random() *255)
+g = Math.round(Math.random() *255)
+b = Math.round(Math.random() *255)
+
+
+this.setAttribute("style", `background-color:rgb(${r}, ${g}, ${b});`)
