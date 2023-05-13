cf61932088f35b1659d5d742192393086dd62cb5
Emii2003 <emile.maia@etec.sp.gov.br>
2023-05-12 21:08:01 -0300

    Criando calculadora

---
+++ js/calc.js
@@ -0,0 +1,31 @@
+//Desafio 1
+//Inserir o value dos botões de números e operações no visor.
+//É obrigatório utilizar funções parametrizadas...
+//Pode ser utilizados eventos de inline.
+
+function insereNumeroNoVisor(botao,visor) {
+    visor.value += botao.value;
+}
+
+//Desafio 2
+//Utilizando a função eval(expressão)
+//Crie uma nova função no formato ArrowFunction que receba um parâmetro 
+//e passe para a função eval() 
+//realizar o cálculo e devolva o resultado no visor.
+
+const calculaResultadoOperacoes = ()=>{
+
+}
+
+//Desafio 3
+//Evite a inserção de mais de um operador por vez no visor.
+function insereNumeroVisor(botao,visor){
+
+    // if (botao.value == "+" || botao.value == "-" || botao.value == "*" || botao.value == "/"){
+    //    visor.value.slice(visor.value.length-1)
+    // } else{
+    //     visor.value += botao.value
+    // }
+    // Terminar esse exc. 
+    //
+}
